c69991f699f8e68e25773e76afdad6135d59aea8
Himesh Rajput <himeshrajput956@gmail.com>
2026-09-15 10:19:34 +0530

    graphql Api with data

---
+++ api module/api_graphql2.js
@@ -0,0 +1,39 @@
+const express=require("express");
+const {graphqlHTTP}=require("express-graphql");
+const {buildSchema}=require("graphql");
+
+const app=express();
+
+const schema=buildSchema(`
+  type Query {
+  message:String
+  }
+  type Student{
+  name:String
+  branch:String
+  age:Int
+  }
+  `);
+
+  const root={
+   student:()=>{
+    return {
+      name:himesh,
+      branch:cse,
+      age:19
+    };
+   }
+  };
+
+  app.use(
+     "/graphql",
+    graphqlHTTP({
+      schema:schema,
+      rootValue:root,
+      graphiql:true
+    })
+  );
+
+  app.listen(3000,()=>{
+    console.log("server is listening at http://localhost:3000/graphql");
+  });
